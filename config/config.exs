2a39a13e35ac7897acb6085f00a17f76368cac32
Andrew Kraut <akraut@gmail.com>
2019-06-19 13:51:29 -0700

    Configure libraries

--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,9 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+config :tesla, adapter: Tesla.Adapter.Hackney
+config :mnesia, dir: '.mnesia/#{Mix.env}/#{node()}'
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
