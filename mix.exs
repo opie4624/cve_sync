b91f795b795bd0a086a4f277c1018a2f29ea61b4
Andrew Kraut <akraut@gmail.com>
2019-06-19 13:51:21 -0700

    Setup dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,11 @@ defmodule CveSync.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:memento, "~> 0.3.1"},
+      {:stream_gzip, "~> 0.4.0"},
+      {:jaxon, "~> 1.0"},
+      {:hackney, "~> 1.15"},
+      {:tesla, "~> 1.2"},
     ]
   end
 end
