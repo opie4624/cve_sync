05f1d292938f3ae57d214529e07354c8e7c45c66
Andrew Kraut <akraut@gmail.com>
2019-06-19 17:00:55 -0700

    Add assigner attribute

--- lib/db/cve.ex
+++ lib/db/cve.ex
@@ -1,4 +1,4 @@
 defmodule CveSync.Db.Cve do
   use Memento.Table,
-    attributes: [:id, :description, :last_modified_date, :published_date]
+    attributes: [:id, :assigner, :description, :last_modified_date, :published_date]
 end
