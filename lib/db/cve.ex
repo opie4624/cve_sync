5935ea2ce901c926b5ba4b6b70403690d2e2e766
Andrew Kraut <akraut@gmail.com>
2019-06-21 10:15:37 -0700

    Create insert helpers for Cve

--- lib/db/cve.ex
+++ lib/db/cve.ex
@@ -1,4 +1,38 @@
 defmodule CveSync.Db.Cve do
   use Memento.Table,
     attributes: [:id, :assigner, :description, :last_modified_date, :published_date]
+
+  alias __MODULE__
+
+  def insert(cve = %Cve{}) do
+    Memento.transaction(fn ->
+      Memento.Query.write(cve)
+    end)
+  end
+
+  def insert(cve) when is_list(cve) do
+    Memento.transaction(fn ->
+      Enum.map(cve, &insert!(&1))
+    end)
+  end
+
+  defp insert!(cve = %Cve{}) do
+    Memento.Query.write(cve)
+  end
+
+  def cast(%{
+        id: id,
+        assigner: assigner,
+        description: description,
+        last_modified_date: last_modified_date,
+        published_date: published_date
+      }) do
+    %Cve{
+      id: id,
+      assigner: assigner,
+      description: description,
+      last_modified_date: last_modified_date,
+      published_date: published_date
+    }
+  end
 end
