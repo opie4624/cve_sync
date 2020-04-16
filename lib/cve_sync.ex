dd64b7d7167ec11aeaf20185be537766d8674ea8
Andrew Kraut <akraut@gmail.com>
2020-04-16 13:57:03 -0700

    This is currently broken.

--- lib/cve_sync.ex
+++ lib/cve_sync.ex
@@ -10,7 +10,7 @@ defmodule CveSync do
     |> create_year_range()
     |> create_filename_list()
     |> fetch_filenames()
-    |> parse_cves()
+    # |> parse_cves()
     |> insert_into_db()
   end
 
@@ -29,7 +29,18 @@ defmodule CveSync do
 
   defp fetch_filenames(filenames), do: Enum.map(filenames, &Downloader.fetch(&1))
 
-  defp parse_cves(x), do: x
+  defp parse_cves(download_streams),
+    do:
+      download_streams
+      |> Enum.map(&extract_cve_lists(&1))
+      |> reduce_to_one_list()
 
   defp insert_into_db(x), do: x
+
+  defp extract_cve_lists(stream),
+    do:
+      stream
+      |> Jaxon.Stream.query([:root, "CVE_Items", :all])
+
+  defp reduce_to_one_list(x), do: x
 end
