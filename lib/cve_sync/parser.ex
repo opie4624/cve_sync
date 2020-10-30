e97786c186e260ad7f251a6eec798b412eddc1d7
Andrew Kraut <akraut@gmail.com>
2020-10-30 15:01:08 -0700

    Parse out description

--- lib/cve_sync/parser.ex
+++ lib/cve_sync/parser.ex
@@ -5,9 +5,16 @@ defmodule CveSync.Parser do
       assigner: cve["cve"]["CVE_data_meta"]["ASSIGNER"],
       affected: cve["cve"]["affects"]["vendor"]["vendor_data"],
       # TODO Filter out English language desc
-      description: nil,
+      description: get_description(cve["cve"]["description"]["description_data"]),
       last_modified_date: cve["lastModifiedDate"],
       published_date: cve["publishedDate"]
     }
   end
+
+  defp get_description(cve) when is_list(cve) do
+    [%{"lang" => "en", "value" => description} | _tail] = cve
+    description
+  end
+
+  defp get_description(_), do: nil
 end
