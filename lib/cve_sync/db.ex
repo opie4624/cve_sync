32ef622631f6a626f4583709af762fa087ca3a04
Andrew Kraut <akraut@gmail.com>
2024-10-14 01:13:43 -0700

    Create helper for transactions

--- lib/cve_sync/db.ex
+++ lib/cve_sync/db.ex
@@ -1,4 +1,14 @@
 defmodule CveSync.Db do
   alias Memento.Query
   alias CveSync.Db.{Cve, Product, Version, Vendor}
+
+  def maybe_transact(f) do
+    case Memento.Transaction.inside?() do
+      false ->
+        Memento.transaction(f)
+
+      _ ->
+        f.()
+    end
+  end
 end
--- lib/cve_sync/db/cve.ex
+++ lib/cve_sync/db/cve.ex
@@ -2,51 +2,54 @@ defmodule CveSync.Db.Cve do
   use Memento.Table,
     attributes: [:id, :assigner, :description, :last_modified_date, :published_date, :raw]
 
+  import CveSync.Db
+
   alias __MODULE__
+  alias Memento.Query
 
   def insert(cve = %Cve{}) do
-    Memento.transaction(fn -> insert!(cve) end)
+    maybe_transact(fn -> insert!(cve) end)
   end
 
   def insert(cve) when is_list(cve) do
-    Memento.transaction(fn ->
+    maybe_transact(fn ->
       Enum.map(cve, &cast(&1))
       |> Enum.map(&insert!(&1))
     end)
   end
 
   defp insert!(cve = %Cve{}) do
-    Memento.Query.write(cve)
+    maybe_transact(fn ->
+      Query.write(cve)
+    end)
   end
 
   def get(cves) when is_list(cves) do
-    Memento.transaction(fn ->
+    maybe_transact(fn ->
       Enum.map(cves, &read!(&1))
     end)
   end
 
   def get(cve_id) do
-    Memento.transaction(fn ->
+    maybe_transact(fn ->
       read!(cve_id)
     end)
   end
 
-  def read!(id), do: Memento.Query.read(__MODULE__, id)
+  def read!(id), do: maybe_transact(fn -> Query.read(__MODULE__, id) end)
+  def read!(id, lock: lock), do: maybe_transact(fn -> Query.read(__MODULE__, id, lock: lock) end)
 
-  def cast(%{
-        id: id,
-        assigner: assigner,
-        description: description,
-        last_modified_date: last_modified_date,
-        published_date: published_date,
-        raw: cve_raw
-      }),
-      do: %Cve{
-        id: id,
-        assigner: assigner,
-        description: description,
-        last_modified_date: last_modified_date,
-        published_date: published_date,
-        raw: cve_raw
-      }
+  def update(cve_id, fields) do
+    f = fn ->
+      read!(cve_id, lock: :write)
+      |> cast(fields)
+      |> insert!
+    end
+
+    maybe_transact(f)
+  end
+
+  def cast(cve \\ %Cve{}, params) do
+    struct(cve, params)
+  end
 end
